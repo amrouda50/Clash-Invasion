54eb0ee4c7e3d35a5e5126872f1d9122bb52e221
Omar Hisham Aly Abdelmeguid Ashour <e5dzst@inf.elte.hu>
2022-03-20 10:17:00 +0200

    More aseprites added

--- assets/Tilesets/BuildingBlocks/block.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.8" tiledversion="1.8.2" name="block" tilewidth="32" tileheight="32" tilecount="1" columns="1">
- <image source="C:/Users/lenovo/Documents/aseprites-project/block.png" width="32" height="32"/>
-</tileset>
