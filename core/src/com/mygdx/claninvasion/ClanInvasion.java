e161602fc9155c0435df73d84ddbae1a405f6610
andreicristea <andrei.cristea@apicore.de>
2022-03-20 13:01:14 +0100

    fix: smaller fixes

--- core/src/com/mygdx/claninvasion/ClanInvasion.java
+++ core/src/com/mygdx/claninvasion/ClanInvasion.java
@@ -2,23 +2,17 @@ package com.mygdx.claninvasion;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.InputProcessor;
-import com.badlogic.gdx.ScreenAdapter;
 import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.maps.tiled.TiledMap;
-import com.badlogic.gdx.maps.tiled.TiledMapRenderer;
-import com.badlogic.gdx.maps.tiled.TmxMapLoader;
-import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+import com.badlogic.gdx.maps.tiled.*;
+import com.badlogic.gdx.maps.tiled.renderers.IsometricTiledMapRenderer;
 
 
 public class ClanInvasion extends ApplicationAdapter {
 	private TiledMap map;
 	private OrthographicCamera camera;
 	private TiledMapRenderer renderer;
-	private InputProcessor inputProcessor;
+	private GameInputProcessor inputProcessor;
 
 	@Override
 	public void create() {
@@ -31,19 +25,19 @@ public class ClanInvasion extends ApplicationAdapter {
 
 		camera.update();
 
-		inputProcessor = new GameInputProcessor();
-
-//		Gdx.app.debug("Location", "Storage path: " + Gdx.files.getLocalStoragePath());
+		inputProcessor = new GameInputProcessor(camera);
 
 		map = new TmxMapLoader().load( Gdx.files.getLocalStoragePath() + "/TileMap/TiledMap.tmx");
-		renderer = new OrthogonalTiledMapRenderer(map);
+		renderer = new IsometricTiledMapRenderer(map);
 		Gdx.input.setInputProcessor(inputProcessor);
 	}
+
 
 	@Override
 	public void render(){
 		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		inputProcessor.onRender();
 
 		camera.update();
 		renderer.setView(camera);
--- core/src/com/mygdx/claninvasion/GameInputProcessor.java
+++ core/src/com/mygdx/claninvasion/GameInputProcessor.java
@@ -1,8 +1,19 @@
 package com.mygdx.claninvasion;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.InputProcessor;
+import com.badlogic.gdx.graphics.Camera;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.math.Vector3;
 
 public class GameInputProcessor implements InputProcessor {
+    private final Camera camera;
+
+    public GameInputProcessor(Camera camera) {
+        this.camera = camera;
+    }
+
     @Override
     public boolean keyDown(int keycode) {
         return false;
@@ -10,7 +21,28 @@ public class GameInputProcessor implements InputProcessor {
 
     @Override
     public boolean keyUp(int keycode) {
+        Vector3 translate = new Vector3(0, 0, 0);
+        if (keycode == Input.Keys.LEFT) {
+            translate.x -= 32;
+        } else if (keycode == Input.Keys.RIGHT) {
+            translate.x += 32;
+        } else if (keycode == Input.Keys.UP) {
+            translate.y -= 32;
+        } else if (keycode == Input.Keys.DOWN) {
+            translate.y += 32;
+        }
+
+        camera.translate(translate);
+
         return false;
+    }
+
+    public void onRender() {
+        if (Gdx.input.isKeyPressed(Input.Keys.Z) && camera instanceof OrthographicCamera) {
+            ((OrthographicCamera)camera).zoom -= 0.01;
+        } else if (Gdx.input.isKeyPressed(Input.Keys.X) && camera instanceof OrthographicCamera) {
+            ((OrthographicCamera)camera).zoom += 0.01;
+        }
     }
 
     @Override
