97c0bef1bd55f71bc7dec7dbca9d9d710f1b7a47
Omar Hisham Aly Abdelmeguid Ashour <e5dzst@inf.elte.hu>
2022-03-20 08:36:03 +0200

    Assets added

--- core/src/com/mygdx/claninvasion/ClanInvasion.java
+++ core/src/com/mygdx/claninvasion/ClanInvasion.java
@@ -1,5 +1,6 @@
 package com.mygdx.claninvasion;
 
+import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
@@ -7,22 +8,24 @@ import com.badlogic.gdx.utils.ScreenUtils;
 
 public class ClanInvasion extends Game {
 	private SpriteBatch batch;
-	private GameScreen gameScreen;
-	
+	private Texture img;
+	private GameScreen gScreen;
 	@Override
 	public void create () {
-		batch = new SpriteBatch( );
-		gameScreen = new GameScreen(batch);
-		setScreen(gameScreen);
+		batch = new SpriteBatch();
+		img = new Texture("cubic.png");
+		gScreen = new GameScreen(batch , img);
+		setScreen(gScreen);
 	}
 
 	@Override
 	public void render () {
-		super.render();
+
 	}
 	
 	@Override
 	public void dispose () {
-
+		batch.dispose();
+		img.dispose();
 	}
 }
--- core/src/com/mygdx/claninvasion/GameScreen.java
+++ core/src/com/mygdx/claninvasion/GameScreen.java
@@ -2,52 +2,44 @@ package com.mygdx.claninvasion;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.ScreenAdapter;
-import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
+import org.w3c.dom.Text;
+import com.badlogic.gdx.graphics.GL20;
+
 
 public class GameScreen extends ScreenAdapter {
-    public static final int WIDTH = 1920 / 3;
-    public static final int HEIGHT = 1080 / 3;
+    public static final int WIDTH = 320*4;
+    public static final int HEIGHT = 180*4;
+    private  SpriteBatch batch;
+    private Texture img;
+    private OrthographicCamera camera;
 
-    private SpriteBatch batch;
-    private OrthographicCamera orthographicCamera;
-    private Renderer renderer;
-
-
-    public GameScreen(SpriteBatch batch) {
+    public GameScreen(SpriteBatch batch , Texture img){
+        this.img = img;
         this.batch = batch;
     }
 
     @Override
-    public void show() {
-        super.show();
-        orthographicCamera = new OrthographicCamera(WIDTH, HEIGHT);
-        orthographicCamera.position.set((float) (WIDTH / 2.0),  (float)(HEIGHT / 2.0), 10);
+    public void show(){
+        //camera = new OrthographicCamera(WIDTH , HEIGHT);
+        //camera.position.set( WIDTH/2 , HEIGHT/2 , 10);
 
-
-        renderer = new Renderer();
     }
-
     @Override
-    public void render(float delta) {
+    public void render(float delta){
         Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-
-        batch.setProjectionMatrix(orthographicCamera.combined);
-
-
-        orthographicCamera.update();
+        batch.setProjectionMatrix(camera.combined);
+     //   camera.update();
         batch.begin();
-
-        renderer.drawGrass(batch);
-
+        batch.draw(img, 0,0 , 50, 50);
         batch.end();
     }
-
     @Override
-    public void dispose() {
-
+    public void dispose(){
     }
+
 }
