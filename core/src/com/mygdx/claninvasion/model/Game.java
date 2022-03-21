06f6170eb1fef4a736800e3be35bcb3e784945fe
Maram <maram4530@hotmail.com>
2022-03-21 19:36:44 +0100

    game

--- core/src/com/mygdx/claninvasion/model/Game.java
+++ core/src/com/mygdx/claninvasion/model/Game.java
@@ -1,4 +1,25 @@
 package com.mygdx.claninvasion.model;
 
-public class Game {
+public class Game{
+    
+    private Player playerOne;
+    private Player playerTwo;
+    private Map map;
+    
+    
+    public void stopGame(){
+    
+    };
+    
+    public void startGame(){
+    
+    };
+    
+    public void changePhase(){
+    
+    };
+    
+    public void changeTurn(){
+    
+    };
 }
