701221d1bb90a0c723909224442fc10120ad60ea
Amina Manzoor Qadir Dinari <qjvad4@inf.elte.hu>
2022-04-06 08:12:04 +0200

    docs: The rest of gamestate package is javadocs-ed

--- core/src/com/mygdx/claninvasion/model/gamestate/EndGameState.java
+++ core/src/com/mygdx/claninvasion/model/gamestate/EndGameState.java
@@ -2,12 +2,28 @@ package com.mygdx.claninvasion.model.gamestate;
 
 import com.mygdx.claninvasion.model.GameModel;
 
+/**
+ * This class is responsible for beginning
+ * of the end state of the game. This is used
+ * when game ends.
+ * @author andreicristea
+ * @author omarashour
+ * @author Dinari
+ * @author Dinari
+ */
 public class EndGameState extends CommonGameState {
+
+    /**
+     * @param game - game model instance
+     */
     public EndGameState(GameModel game) {
         super(game);
         this.game.setGameEnded(true);
     }
 
+    /**
+     * Changes the state of the game
+     */
     @Override
     public void changeState() {
         this.game.setGameState(new StartGameState(game));
