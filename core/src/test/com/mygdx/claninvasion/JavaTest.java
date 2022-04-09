969f9221834fccaa53a81e7b6268c23408282105
Omar Hisham Aly Abdelmeguid Ashour <e5dzst@inf.elte.hu>
2022-04-09 20:08:27 +0200

    Testcase added

---
+++ core/src/test/com/mygdx/claninvasion/JavaTest.java
@@ -0,0 +1,16 @@
+package com.mygdx.claninvasion;
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class JavaTest {
+
+    @org.junit.Test
+    public void oneEqualsOne() throws Exception {
+        //throw new Exception("");
+        //System.out.println(50000);
+
+        assertEquals(1, 1);
+    }
+
+}
