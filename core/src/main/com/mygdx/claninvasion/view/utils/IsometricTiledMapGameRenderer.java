508b5148e6bef558ee8e300a55e6fd3f6eb69ba0
Cristea Andrei <w61rab@inf.elte.hu>
2022-04-13 05:18:01 +0200

    fix: map != null case in renderTileCell

--- core/src/main/com/mygdx/claninvasion/view/utils/IsometricTiledMapGameRenderer.java
+++ core/src/main/com/mygdx/claninvasion/view/utils/IsometricTiledMapGameRenderer.java
@@ -237,7 +237,7 @@ public class IsometricTiledMapGameRenderer extends BatchTiledMapRenderer {
                 } catch (Exception ignored) {
 
                 }
-            } else {
+            } else if (map != null) {
                 throw new UnknownTiledMapLayerException(layer);
             }
         }
