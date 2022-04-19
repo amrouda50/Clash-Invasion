179d0a5b022b9ab427581facc5bb7edf71399f6c
Amina Manzoor Qadir Dinari <qjvad4@inf.elte.hu>
2022-04-19 03:19:26 +0200

    LevelIterateTower

--- core/src/main/com/mygdx/claninvasion/view/applicationlistener/MainGamePageUI.java
+++ core/src/main/com/mygdx/claninvasion/view/applicationlistener/MainGamePageUI.java
@@ -219,6 +219,7 @@ public final class MainGamePageUI implements ApplicationListener {
                 if (!player.equals(app.getCurrentPlayer())) {
                     return;
                 }
+                System.out.println(selectBox.getSelected().split(" ")[1]);
                 switch (selectBox.getSelected().split(" ")[1]) {
                     case "Tower":
                         if (player.canCreateTower()) {
@@ -237,6 +238,11 @@ public final class MainGamePageUI implements ApplicationListener {
                         }
                         break;
                     case "Level":
+                        System.out.println("You are about to update");
+                        if(player.canUpdateLevel()) {
+                            System.out.println("You have funds to update");
+                            player.levelUp();
+                        }
                         break;
                     case "Dragon":
                         createDragon(player, table);
