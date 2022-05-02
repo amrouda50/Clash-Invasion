85603bdf9449786ffad0425137a09ab114c4559b
Omar Hisham Aly Abdelmeguid Ashour <e5dzst@inf.elte.hu>
2022-05-02 04:54:17 +0200

    Battle state class fireTower method is changed to make the Tower hit

---
+++ assets/Tilesets/Buildings/splashtower.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.2" name="splashtower" tilewidth="20" tileheight="47" tilecount="1" columns="1">
+ <image source="../../Buidlings/splashtower.png" width="20" height="47"/>
+</tileset>
