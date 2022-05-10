32999546777c253f9567714c0da551b5cace9aa1
Cristea Andrei <w61rab@inf.elte.hu>
2022-05-10 13:07:33 +0200

    fix: code styling

--- core/src/test/com/mygdx/claninvasion/ArtificialEntityTest.java
+++ core/src/test/com/mygdx/claninvasion/ArtificialEntityTest.java
@@ -1,25 +1,9 @@
 package com.mygdx.claninvasion;
-import com.badlogic.gdx.Screen;
-import com.mygdx.claninvasion.model.entity.Barbarian;
-import com.mygdx.claninvasion.model.entity.Entity;
-import com.mygdx.claninvasion.model.entity.EntitySymbol;
-import com.mygdx.claninvasion.model.map.WorldCell;
-import com.mygdx.claninvasion.model.map.WorldMap;
-import com.mygdx.claninvasion.view.screens.GamePage;
-import com.mygdx.claninvasion.view.screens.GameScreens;
-import com.mygdx.claninvasion.view.screens.GameScreensManager;
-import org.javatuples.Pair;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.junit.MockitoJUnitRunner;
-
-import java.util.ArrayList;
-import java.util.EmptyStackException;
-import java.util.NoSuchElementException;
-
-import static org.mockito.Mockito.mock;
 
 @RunWith(MockitoJUnitRunner.class)
 public class ArtificialEntityTest {
@@ -30,7 +14,7 @@ public class ArtificialEntityTest {
 
     @Test
     public void TestConstructor(){
-        Assert.assertEquals(true , true);
+        Assert.assertTrue(true);
     }
 
 
--- core/src/test/com/mygdx/claninvasion/EntityTest.java
+++ core/src/test/com/mygdx/claninvasion/EntityTest.java
@@ -1,23 +1,14 @@
 package com.mygdx.claninvasion;
-import com.badlogic.gdx.Screen;
-import com.mygdx.claninvasion.model.entity.Barbarian;
 import com.mygdx.claninvasion.model.entity.Entity;
 import com.mygdx.claninvasion.model.entity.EntitySymbol;
 import com.mygdx.claninvasion.model.map.WorldCell;
 import com.mygdx.claninvasion.model.map.WorldMap;
-import com.mygdx.claninvasion.view.screens.GamePage;
-import com.mygdx.claninvasion.view.screens.GameScreens;
-import com.mygdx.claninvasion.view.screens.GameScreensManager;
 import org.javatuples.Pair;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.junit.MockitoJUnitRunner;
-
-import java.util.ArrayList;
-import java.util.EmptyStackException;
-import java.util.NoSuchElementException;
 
 import static org.mockito.Mockito.mock;
 
@@ -38,9 +29,9 @@ public class EntityTest {
     }
 
     @Test
-    public void TestGettersAndConstructor(){
-        Pair<Integer , Integer> position = new Pair<Integer , Integer>(5 , 5);
-        x = new Entity(EntitySymbol.BARBARIAN , new Pair<Integer, Integer>(5 , 5) , 12);
+    public void testGettersAndConstructor(){
+        Pair<Integer , Integer> position = new Pair<>(5, 5);
+        x = new Entity(EntitySymbol.BARBARIAN , new Pair<>(5, 5) , 12);
         //test symbol constructor functionality and getter symbol
         Assert.assertEquals(x.getSymbol() ,  EntitySymbol.BARBARIAN);
         //test x axis position
@@ -49,13 +40,14 @@ public class EntityTest {
         Assert.assertEquals(x.getPositionY() , position.getValue1());
     }
     @Test(expected = IndexOutOfBoundsException.class)
-    public void TestPositionOutOfBounds(){
-        x = new Entity(EntitySymbol.BARBARIAN , new Pair<Integer, Integer>(32 , 12) , 12);
+    public void testPositionOutOfBounds(){
+        x = new Entity(EntitySymbol.BARBARIAN , new Pair<>(32, 12) , 12);
     }
+
     @Test
-    public void TestPositionInBounds(){
-        x = new Entity(EntitySymbol.BARBARIAN , new Pair<Integer, Integer>(12 , 12) , 12);
-        Entity y = new Entity(EntitySymbol.BARBARIAN , new Pair<Integer, Integer>(0, 0) , 12);
-        Entity z = new Entity(EntitySymbol.BARBARIAN , new Pair<Integer, Integer>(6, 6) , 12);
+    public void testPositionInBounds(){
+        x = new Entity(EntitySymbol.BARBARIAN , new Pair<>(12, 12) , 12);
+        Entity y = new Entity(EntitySymbol.BARBARIAN , new Pair<>(0, 0) , 12);
+        Entity z = new Entity(EntitySymbol.BARBARIAN , new Pair<>(6, 6) , 12);
     }
 }
