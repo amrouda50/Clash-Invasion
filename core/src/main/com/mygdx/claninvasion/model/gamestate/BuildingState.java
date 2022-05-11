8f22db790342d90345dd9f5c749cead66a1ddc11
Cristea Andrei <w61rab@inf.elte.hu>
2022-05-11 07:56:41 +0200

    fix: prepare globals for the release

--- core/src/main/com/mygdx/claninvasion/model/gamestate/BuildingState.java
+++ core/src/main/com/mygdx/claninvasion/model/gamestate/BuildingState.java
@@ -15,7 +15,7 @@ import java.util.TimerTask;
  * @author Dinari
  */
 public class BuildingState extends CommonGameState implements Building {
-    private static final int COUNTER_INIT = 10;
+    private static final int COUNTER_INIT = 30;
     private static final int END_PHASE_TIME = COUNTER_INIT * 2;
     private float timeSeconds = 0f;
     private final float diff = 1f;
