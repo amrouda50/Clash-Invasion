74f5b03e67a630a417a17aae41db676a19ad2445
Amina Manzoor Qadir Dinari <qjvad4@inf.elte.hu>
2022-05-11 03:21:36 +0200

    Inner logic of artillery for towers complete

--- core/src/main/com/mygdx/claninvasion/model/entity/attacktype/AttackTypeArtillery.java
+++ core/src/main/com/mygdx/claninvasion/model/entity/attacktype/AttackTypeArtillery.java
@@ -5,9 +5,32 @@ package com.mygdx.claninvasion.model.entity.attacktype;
  * TODO: Logic part is missing
  */
 public class AttackTypeArtillery extends AttackTypeTower {
+
+    public AttackTypeArtillery() {
+    }
+
+    @Override
+    public int getDecreaseHealth() {
+        return super.getDecreaseHealth();
+    }
+
+    @Override
+    public void setDecreaseHealth(int decreaseHealth) {
+        super.setDecreaseHealth(decreaseHealth);
+    }
+
     /**
      * Overrides base attack implementation
      * @see AttackType
      */
 
+    @Override
+    public void attack() {
+        setDecreaseHealth(85);
+    }
+
+    @Override
+    public Attacks getName() {
+        return super.getName();
+    }
 }
